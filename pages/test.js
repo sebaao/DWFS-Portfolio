ef23dfc32ed4ffae8a75e54b851db90e16978c75
sebaao <saburto11@gmail.com>
2024-03-25 02:01:58 -0300

    m

--- pages/test.js
+++ pages/test.js
@@ -3,7 +3,7 @@ import React from 'react';
 const ImageWithOverlay = ({ src, alt, overlayText }) => {
   return (
     <div className="relative pb-full">
-      <img src={src} className="rounded-lg object-cover w-full h-64" alt={alt} />
+      <img src={`/public/${src}`} className="rounded-lg object-cover w-full h-64" alt={alt} />
       <div className="absolute bottom-0 left-0 w-full h-full bg-black opacity-0">
         <div className="px-4 py-4 text-white text-center">
           <h2 className="text-xl font-medium">{overlayText}</h2>
